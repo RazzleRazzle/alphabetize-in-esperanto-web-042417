54d2ba9f106666e91263ae6012902a6d8f7a3bd7
Jake Musich <makejusich@gmail.com>
2017-04-23 08:13:24 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,11 @@
+require 'pry'
+
 def alphabetize(arr)
-  # code here
+alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by do |item|
+    item.split('').collect do |l|
+      alphabet.index(l)
+    end
+  end
+  #binding.pry
 end
